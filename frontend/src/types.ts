2c99db4bf7b96bab0c32ebe47f6985bdbcf94e07
Daniel Pointon <dlp14developing@gmail.com>
2024-11-16 19:53:12 +0000

    Reformat

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,14 +1,13 @@
 // Define the structure of the event
 export interface Event {
-    timestamp: string;
-    description: string;
-  }
-  
-  // Define the structure of the claim details
-  export interface ClaimDetails {
-    date: string;
-    description: string;
-    id: string;
-    events: Event[];
-  }
-  
+  timestamp: string;
+  description: string;
+}
+
+// Define the structure of the claim details
+export interface ClaimDetails {
+  date: string;
+  description: string;
+  id: string;
+  events: Event[];
+}
